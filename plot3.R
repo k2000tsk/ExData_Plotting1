6255d7691770c54ab21cc84b3a9735fde02cb029
k2000tsk <positivets@gmail.com>
2017-06-26 10:59:36 +0900

    plot3 R code

---
+++ plot3.R
@@ -0,0 +1,28 @@
+
+# Load file "household_power_consumption.txt"
+# There are 2,075,259 Lines & 9 Columns
+power_data<-read.table(file="household_power_consumption.txt",sep=";",
+                       na.strings="?",header=TRUE)
+
+# Subtract the data from 2007-02-01 ~ 2007-02-02
+# There are 2,880 Lines $ 9 Columns
+ valid_power_data<-power_data[(power_data$Date=="1/2/2007")|(power_data$Date=="2/2/2007"),]
+
+
+# add new column in last
+# date+time is added in the last column
+valid_power_data$DateTime<-strptime(paste(valid_power_data[,1],valid_power_data[,2],
+                                          sep=""),format="%d/%m/%Y %H:%M:%S")
+
+# draw a graph
+png("plot3.png",width=480,height=480,units="px")
+
+# draw 3 graphs of sub_metering 
+plot(valid_power_data$DateTime,valid_power_data$Sub_metering_1,type="l",
+     xlab="",ylab="Energy sub metering")
+lines(valid_power_data$DateTime,valid_power_data$Sub_metering_2,col="red")
+lines(valid_power_data$DateTime,valid_power_data$Sub_metering_3,col="blue")
+legend("topright",col=c("black","red","blue"),
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1)
+
+dev.off()
